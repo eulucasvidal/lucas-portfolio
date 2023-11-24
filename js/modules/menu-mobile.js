eaacdd6a01e801d17c8fb0b72fd6dab92e39a8f5
Lucas Vidal <luccas.lima12@gmail.com>
2023-11-24 08:05:33 -0300

    Implementando menu interativo com JS

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,11 @@
+export default function initMenuMobile() {
+  const botaoMenu = document.querySelector('[data-menu="button"]');
+  const menuMobile = document.querySelector('[data-menu="suave"]');
+
+  function openMenu(event) {
+    botaoMenu.classList.toggle("ativo");
+    menuMobile.classList.toggle("ativo");
+  }
+
+  botaoMenu.addEventListener("click", openMenu);
+}
